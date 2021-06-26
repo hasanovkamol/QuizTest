80d3328cca3da8e6d83371feb241030b159eaa5d
KamolHasanov <khasanovkamol3834@gmail.com>
2021-06-26 16:24:35 +0500

    add conpanenta

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QustionComponent } from './qustion/qustion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QustionComponent
   ],
   imports: [
     BrowserModule,
