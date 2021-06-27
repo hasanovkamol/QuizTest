4e007f793dc5f47ec988c495c5e965d83513cfb6
KamolHasanov <khasanovkamol3834@gmail.com>
2021-06-27 18:13:40 +0500

    change code

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QustionComponent } from './qustion/qustion.component';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   declarations: [
     AppComponent,
     QustionComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
